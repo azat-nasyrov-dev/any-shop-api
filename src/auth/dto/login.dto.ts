9af6b7eebb39eb44c765bc187da554a02f977296
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-05-31 21:48:17 +0600

    added login dto

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class LoginDto {
+  @IsNotEmpty({ message: "The user's email must not be empty" })
+  @IsEmail()
+  @IsString()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(12)
+  @IsString()
+  readonly password: string;
+}
